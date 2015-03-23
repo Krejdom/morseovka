10ba2a7e66593dfb94e56896fdd421599f6eca6b
Dominika Krejčí <krejci.dominika@seznam.cz>
2015-03-23 17:27:33 +0100

    první commit

---
+++ run.py
@@ -0,0 +1,79 @@
+print("------------------------------------------------------------")
+print("                Překladač Morseovy abecedy                ")
+print()
+
+print("Pro překlad Morseovky na text napiš 'M',")
+print("pro překlad textu do morseovky napiš 'T': ")
+
+co = input().upper()
+
+def JakyPreklad(co):
+	if co == 'M':
+		return MorseToText(zprava)
+	elif co == 'T':
+		return TextToMorse(zprava)
+	else:
+		pass
+print()
+
+def UpravaZpravy(zprava):
+	zprava = zprava.lower()
+	zprava = zprava.replace("ě","e")
+	zprava = zprava.replace("š", "s")
+	zprava = zprava.replace("č", "c")
+	zprava = zprava.replace("ř", "r")
+	zprava = zprava.replace("ž", "z")
+	zprava = zprava.replace("ý", "y")
+	zprava = zprava.replace("á", "a")
+	zprava = zprava.replace("í", "i")
+	zprava = zprava.replace("é", "e")
+	zprava = zprava.replace("ů", "u")
+	zprava = zprava.replace("ú", "u")
+	zprava = zprava.replace("ó", "o")
+	return zprava
+
+zprava = UpravaZpravy(input("Zadej text, který chceš přeložit: "))
+
+slovnik = {'.-' : 'a', '-...' : 'b', '-.-.' : 'c', '-..' : 'd', '.' : 'e', '..-.' : 'f', '--.' : 'g', '....' : 'h', '----' : 'ch', '..' : 'i', '.---' : 'j', '-.-' : 'k', '.-..' : 'l', '--' : 'm', '-.' : 'n', '---' : 'o', '.--.' : 'p', '--.-' : 'q', '.-.': 'r', '...' : 's', '-' : 't', '..-' : 'u', '...-' : 'v', '.--' : 'w', '-..-' : 'x', '-.--' : 'y', '--..' : 'z',  '.-.-.-' : '. ',  '' : ' ', '--..--' : ',', '..--..' : '?'}
+#slovnik2 = {'a': '.-', 'b' : '-...', 'c' : '-.-.', 'd' : '-..', 'e' : '.', 'f' : '..-.', 'g' : '--.', 'h' : '....', 'ch' : '----','i' : '..','j' : '.---', 'k' : '-.-', 'l' : '.-..', 'm' : '--', 'n' : '-.', 'o' : '---', 'p' : '.--.', 'q' : '--.-', 'r' : '.-.', 's' : '...', 't' : '-', 'u' : '..-', 'v' : '...-', 'w' : '.--', 'x' : '-..-', 'y' : '-.--', 'z' : '--..', ' ' : '', '.' : '//', '?' : '..--..', ',' : '--..--'}
+
+slovnik2 = {}
+for k, v in slovnik.items():
+	slovnik2[v] = k
+
+slovnik2 = {v:k for k, v in slovnik.items()}
+
+
+# z morseovky na text
+def MorseToText(zprava):
+	zprava = zprava.replace('///', '/.-.-.-/')
+	zprava = zprava.replace('//', '//')
+	zprava = zprava.split("/")
+
+	morse = []
+	for i,znak in enumerate(zprava):
+		pismeno = slovnik.get(znak)
+		morse.append(pismeno)
+	if morse[-1] == None:
+		morse.pop(-1)
+	morse = ''.join(morse)
+
+	return morse
+
+# z textu na morseovku
+def TextToMorse(zprava):
+	text = ""
+	for znak in zprava:
+		text += slovnik2.get(znak)
+		text += "/"
+	while text[-3:] != "///":
+		text += "/"
+
+	return text
+
+print()
+print("Překlad:", JakyPreklad(co))
+
+
+import sys
+print(sys.argv)
