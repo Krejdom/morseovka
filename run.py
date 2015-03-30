62db7861a310fbe92b0f471b12ef1939e573fa03
Dominika Krejčí <krejci.dominika@seznam.cz>
2015-03-30 17:46:21 +0200

    přidání testů

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import sys
-#print(sys.argv[1])
+from morseovka import MorseToText, TextToMorse
 
 if len(sys.argv) == 1:
 	print("usage: morse [M|T]", file=sys.stderr)
@@ -17,59 +17,7 @@ def JakyPreklad(co):
 	else:
 		pass
 
-def UpravaZpravy(zprava):
-	zprava = zprava.lower()
-	zprava = zprava.replace("ě","e")
-	zprava = zprava.replace("š", "s")
-	zprava = zprava.replace("č", "c")
-	zprava = zprava.replace("ř", "r")
-	zprava = zprava.replace("ž", "z")
-	zprava = zprava.replace("ý", "y")
-	zprava = zprava.replace("á", "a")
-	zprava = zprava.replace("í", "i")
-	zprava = zprava.replace("é", "e")
-	zprava = zprava.replace("ů", "u")
-	zprava = zprava.replace("ú", "u")
-	zprava = zprava.replace("ó", "o")
-	return zprava
-
-zprava = UpravaZpravy(input())
-
-slovnik = {'.-' : 'a', '-...' : 'b', '-.-.' : 'c', '-..' : 'd', '.' : 'e', '..-.' : 'f', '--.' : 'g', '....' : 'h', '----' : 'ch', '..' : 'i', '.---' : 'j', '-.-' : 'k', '.-..' : 'l', '--' : 'm', '-.' : 'n', '---' : 'o', '.--.' : 'p', '--.-' : 'q', '.-.': 'r', '...' : 's', '-' : 't', '..-' : 'u', '...-' : 'v', '.--' : 'w', '-..-' : 'x', '-.--' : 'y', '--..' : 'z',  '.-.-.-' : '. ',  '' : ' ', '--..--' : ',', '..--..' : '?'}
-
-slovnik2 = {}
-for k, v in slovnik.items():
-	slovnik2[v] = k
-
-slovnik2 = {v:k for k, v in slovnik.items()}
-
-
-# z morseovky na text
-def MorseToText(zprava):
-	zprava = zprava.replace('///', '/.-.-.-/')
-	zprava = zprava.replace('//', '//')
-	zprava = zprava.split("/")
-
-	morse = []
-	for i,znak in enumerate(zprava):
-		pismeno = slovnik.get(znak)
-		morse.append(pismeno)
-	if morse[-1] == None:
-		morse.pop(-1)
-	morse = ''.join(morse)
-
-	return morse
-
-# z textu na morseovku
-def TextToMorse(zprava):
-	text = ""
-	for znak in zprava:
-		text += slovnik2.get(znak)
-		text += "/"
-	while text[-3:] != "///":
-		text += "/"
-
-	return text
+zprava = input()
 
 print()
 print(JakyPreklad(co))
---
+++ test_morse.py
@@ -0,0 +1,4 @@
+import morseovka
+
+def test_A():
+	assert morseovka.TextToMorse("A") == ".-///"
